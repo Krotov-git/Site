86eb19bf2b5b6833fc14237aff661b483495547c
Krotov-git <krotov.ne@gmail.com>
2021-09-20 19:23:06 +0300

    адаптив попытка на сервер

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, TestPageView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
-    path('', HomePageView.as_view(), name='home'),
+    path('', TestPageView.as_view(), name='test'),
+    path('home/', HomePageView.as_view(), name='home'),
 ] # Данный паттерн практически полностью идентичен тому, что мы использовали в прошлом уроке,
     # однако есть одно важное отличие:
     # во время использования классовых представлений в конце названия представления добавляется as_view().
